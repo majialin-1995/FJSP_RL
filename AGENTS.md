fcb4091f6939d365070319ce72c2e23678391e15
majialin-1995 <jialinm@lut.edu.cn>
2025-10-31 12:32:33 +0800

    update

---
+++ AGENTS.md
@@ -0,0 +1,6 @@
+# 混合增强的智能农机任务协同规划方法研究
+
+以FJSP为问题背景，混合增强的智能农机任务协同规划方法研究
+
+pip install job-shop-lib
+pip install "stable-baselines3[extra]" gymnasium
---
+++ main.py
@@ -0,0 +1,8 @@
+from job_shop_lib.benchmarking import load_benchmark_instance
+from job_shop_lib.reinforcement_learning import (
+    SingleJobShopGraphEnv,
+    MultiJobShopGraphEnv,
+    ObservationDict,
+    ObservationSpaceKey,
+    IdleTimeReward,
+)
